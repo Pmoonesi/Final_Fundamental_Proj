e3a6516ed039d130b625bbb353b8464f878cc069
P_Moonesi <pmoonesi@aut.ac.ir>
2020-01-27 17:44:28 +0330

    main program is almost done, no graphics yet, save and load needs debug

--- main.c
+++ main.c
@@ -1,89 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "nodeVacell.h"
+#include "saveVaload.h"
 
+void play_that(node** head,cell** cell_head1,cell** cell_head2,int ** ma_p,int size,int player,bool* loop)
+{
+    printf("PLAYER%d 's turn\n",player);
+    int p,i,j;
+    show_map(ma_p,size);
+    printf("which one of the following cells ?\n");
+    print_cells(*cell_head1);
+    scanf("%d",&p);
+    printf("[1]move cell\n[2]split cell\n[3]boost energy\n[4]save\n[5]exit\n");
+    scanf("%d",&i);
+    if (i==1)
+    {
+        printf("in which direction ?\n[1]North\n[2]South\n[3]Northeast\n[4]Northwest\n[5]Southeast\n[6]Southwest\n");
+        scanf("%d",&j);
+        cell_move(*cell_head1,p,j);
+    }
+    else if (i==2)
+    {
+        split_cell(cell_head1,p);
+    }
+    else if (i==3)
+    {
+        gain_energy(*cell_head1,p);
+    }
+    else if (i==4)
+    {
+        if (player==1)
+            save_game(*head,*cell_head1,*cell_head2,"map.bin",ma_p,size);
+        else 
+            save_game(*head,*cell_head2,*cell_head1,"map.bin",ma_p,size);
+    }
+    else if (i==5){
+        *loop=false;
+    }
+    else 
+        printf("invalid input\n");
+}
 
 int main()
 {
     srand(time(0));
-    bool loop=true;
-    int p;
-    int i,j,n,c;
-    scanf("%d",&n);
-    int **ma_p;
-    ma_p=(int**)malloc(n*sizeof(int*));
-    for (i=0;i<n;i++)
-    {
-        ma_p[i]=(int*)malloc(n*sizeof(int));
-        for (j=0;j<n;j++){
-            ma_p[i][j]=rand()%4+1;
-            printf("%d ",ma_p[i][j]);
-        }
-        printf("\n");
-    }
-    node* head=make_network(ma_p,n);
+    bool outloop=true;
     
-    scanf("%d",&c);
-    cell* cell_head=create_cell(c,n,head);
+    while(outloop){
+        bool loop=true;
+        int size,cell_num;
 
-    /*while (loop)
-    {
-        printf("[1]move cell\n[2]split cell\n[3]boost energy\n[4]exit\n");
-        scanf("%d",&i);
-        if (i==1)
+        int choice,num_players;
+        printf("[1]Load\n[2]New single player game\n[3]New multiplayer game\n[4]Exit\n");
+        scanf("%d",&choice);
+        int **ma_p;
+        cell *cell_head1=NULL;
+        cell *cell_head2=NULL;
+        node *head;
+        if (choice==1)
         {
-            printf("which one of the following cells ?\n");
-            print_cells(cell_head);
-            scanf("%d",&p);
-            printf("in which direction ?\n[1]North\n[2]South\n[3]Northeast\n[4]Northwest\n[5]Southeast\n[6]Southwest\n");
-            scanf("%d",&j);
-            cell_move(cell_head,p,j);
+            printf("[1]single player save\n[2]multiplayer save\n");
+            scanf("%d",&num_players);
+            load_game(&head,&cell_head1,&cell_head2,&ma_p,num_players);
         }
-        else if (i==2)
+        else if (choice==2)
         {
-            printf("which one of the following cells ?\n");
-            print_cells(cell_head);
-            scanf("%d",&p);
-            split_cell(&cell_head,p);
+            FILE* fp=fopen("map.bin","rb");
+            fread(&size,sizeof(int),1,fp);
+            ma_p=read_file(fp);
+            fclose(fp);
+            head=make_network(ma_p,size);
+            printf("please enter initial number of cells : ");
+            scanf("%d",&cell_num);
+            cell_head1=create_cell(cell_num,size,head);
         }
-        else if (i==3)
+        else if (choice==3)
         {
-            printf("which one of the following cells ?\n");
-            print_cells(cell_head);
-            scanf("%d",&p);
-            gain_energy(cell_head,p);
+            FILE* fp=fopen("map.bin","rb");
+            fread(&size,sizeof(int),1,fp);
+            ma_p=read_file(fp);
+            fclose(fp);
+            head=make_network(ma_p,size);
+            printf("PLAYER1 : please enter initial number of cells : ");
+            scanf("%d",&cell_num);
+            cell_head1=create_cell(cell_num,size,head);
+            printf("PLAYER2 : please enter initial number of cells : ");
+            scanf("%d",&cell_num);
+            cell_head2=create_cell(cell_num,size,head);
         }
-        else if (i==4)
+        else if (choice==4)
+        {
             loop=false;
-        else 
-            printf("invalid input\n");
-    }*/
-    while (loop)
-    {
-        printf("which one of the following cells ?\n");
-        print_cells(cell_head);
-        scanf("%d",&p);
-        printf("[1]move cell\n[2]split cell\n[3]boost energy\n[4]exit\n");
-        scanf("%d",&i);
-        if (i==1)
-        {
-            printf("in which direction ?\n[1]North\n[2]South\n[3]Northeast\n[4]Northwest\n[5]Southeast\n[6]Southwest\n");
-            scanf("%d",&j);
-            cell_move(cell_head,p,j);
+            outloop=false;
         }
-        else if (i==2)
+        while (loop)
         {
-            split_cell(&cell_head,p);
+            play_that(&head,&cell_head1,&cell_head2,ma_p,size,1,&loop);
+            if (loop==false)
+                break;
+            if(cell_head2!=NULL)
+                play_that(&head,&cell_head2,&cell_head1,ma_p,size,2,&loop);
         }
-        else if (i==3)
-        {
-            gain_energy(cell_head,p);
-        }
-        else if (i==4)
-            loop=false;
-        else 
-            printf("invalid input\n");
     }
     return 0;
 }
